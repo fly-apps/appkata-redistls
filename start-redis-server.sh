34c51accb0839c625fa0c6cd3a604bbd54dc6c5a
Dj Walker-Morgan <codepope@gmail.com>
2020-11-19 17:39:07 +0000

    Update with TLS configuration/readme

--- start-redis-server.sh
+++ start-redis-server.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 sysctl vm.overcommit_memory=1
 sysctl net.core.somaxconn=1024
+
+# pass env vars as server args
 redis-server /usr/local/etc/redis/redis.conf --requirepass $REDIS_PASSWORD
